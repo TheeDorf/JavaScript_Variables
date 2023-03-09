b14a945c96769d2d6c3bb4d5c0fff1d77446b602
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-09 09:23:09 -0800

    complete the 5 exercises

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "AJ";
+let lastName;
+var age; 
 // Exercise 2
-
+lastName = "Dorfman";
+age = 32;
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let slectedSize= "M";
+
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true
+
+console.log(firstName)
